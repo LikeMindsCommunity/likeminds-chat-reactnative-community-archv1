167da9f170616194408e045b1c738e20b63ea965
likeminds <likeminds@192.168.0.171>
2023-04-13 12:36:53 +0530

    updated versionCode and and versionName

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
 
 export const myClient = new LikeMinds({
   apiKey: '',
-  xVersionCode:'4',
+  xVersionCode:'5',
   xPlatformCode: 'rn',
 });
 
