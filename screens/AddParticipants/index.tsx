7bbc57a990ef767e60371a3518dc65eb69559151
likeminds <likeminds@likemindss-MacBook-Pro-3.local>
2023-04-27 17:33:10 +0530

    removed unneccessary imports

--- screens/AddParticipants/index.tsx
+++ screens/AddParticipants/index.tsx
@@ -1,20 +1,4 @@
-import {
-  View,
-  Text,
-  Image,
-  TouchableOpacity,
-  ScrollView,
-  FlatList,
-  ActivityIndicator,
-  TextInput,
-} from 'react-native';
-import React, {useEffect, useLayoutEffect, useState} from 'react';
-import {styles} from './styles';
-import STYLES from '../../constants/Styles';
-import {myClient} from '../..';
-import {StackActions} from '@react-navigation/native';
-import {SHOW_TOAST} from '../../store/types/types';
-import {useAppDispatch} from '../../store';
+import React from 'react';
 import CommonAllMembers from '../../components/CommonAllMembers';
 
 const AddParticipants = ({navigation, route}: any) => {
