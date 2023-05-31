26938b149669ecfb65c1f6446f7f2991b013dee1
likeminds <likeminds@192.168.0.171>
2023-06-01 00:18:16 +0530

    removed volume bar from video player

--- screens/FIleUpload/index.tsx
+++ screens/FIleUpload/index.tsx
@@ -285,6 +285,7 @@ const FileUpload = ({navigation, route}: any) => {
               videoStyle={styles.videoPlayer}
               videoRef={video}
               disableBack={true}
+              disableVolume={true}
               disableFullscreen={true}
               paused={true}
             />
