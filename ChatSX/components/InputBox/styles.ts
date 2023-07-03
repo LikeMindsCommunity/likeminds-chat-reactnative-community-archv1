db3948d156b930698b967d87a459428fe512ae76
likeminds <likeminds@likemindss-MacBook-Pro-3.local>
2023-07-03 14:02:35 +0530

    Resolved: Border alignment issue on Inputbox when we type long message

--- ChatSX/components/InputBox/styles.ts
+++ ChatSX/components/InputBox/styles.ts
@@ -9,10 +9,14 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     flexGrow: 1,
     borderRadius: 30,
-    overflow: 'hidden',
     width: Layout.window.width - 75,
-    borderColor: STYLES.$COLORS.MSG,
-    borderWidth: 0.2,
+  },
+  inputBoxWithShadow: {
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 1},
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
   },
   inputContainer: {
     display: 'flex',
