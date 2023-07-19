4b3e59d9b8fd2bf68904047e9a84f4778d8aa666
likeminds <likeminds@likemindss-MacBook-Pro-3.local>
2023-07-19 19:57:27 +0530

    Added: function info in a comment

--- ChatSX/commonFuctions/index.tsx
+++ ChatSX/commonFuctions/index.tsx
@@ -477,6 +477,7 @@ export const formatValue = (value: any) => {
   return '';
 };
 
+// this function is used to extract path from from route query, i.e routeQuery: `user_profile/skjdnc-lskdnjcs-lkdnsm`, path: `skjdnc-lskdnjcs-lkdnsm`
 export function extractPathfromRouteQuery(inputString: string): string | null {
   const match = inputString.match(EXTRACT_PATH_FROM_ROUTE_QUERY);
   if (match && match[1]) {
