408c793a67d3c1fc5ca8fdf1c3d3a191f558ce11
arnavr15 <arnav.ranjan@likeminds.community>
2023-08-09 22:36:31 +0530

    changed versions

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
 
 export const myClient = LMChatClient.setApiKey('')
   .setPlatformCode('rn')
-  .setVersionCode(parseInt('13'))
+  .setVersionCode(parseInt('14'))
   .build();
 
 function HeadlessCheck({isHeadless}) {
