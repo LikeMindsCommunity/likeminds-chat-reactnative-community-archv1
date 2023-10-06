d02ebdd7a69efb533acf30aaa46a30120f9f6a67
likeminds <likeminds@likemindss-MacBook-Pro-3.local>
2023-10-06 21:10:22 +0530

    added separate InputBoxProps model

---
+++ ChatSX/components/InputBox/models/InputBoxProps.ts
@@ -0,0 +1,17 @@
+export interface InputBoxProps {
+  replyChatID?: any;
+  chatroomID: any;
+  chatRequestState?: any;
+  chatroomType?: any;
+  navigation: any;
+  isUploadScreen: boolean;
+  isPrivateMember?: boolean;
+  isDoc?: boolean;
+  myRef?: any;
+  previousMessage?: string;
+  handleFileUpload: any;
+  isEditable?: boolean;
+  setIsEditable?: any;
+  isSecret?: any;
+  chatroomWithUser?: any;
+}
