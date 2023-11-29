38a09be51518af9f97af13d338116b53637d5401
arnavr15 <arnav.ranjan@likeminds.community>
2023-11-29 13:24:23 +0530

    fixed linting issues

--- ChatSX/LMChatApp.tsx
+++ ChatSX/LMChatApp.tsx
@@ -1,16 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {KeyboardAvoidingView, Linking, Platform} from 'react-native';
+import React, {useEffect} from 'react';
+import {KeyboardAvoidingView, StyleSheet, Platform} from 'react-native';
 import {Provider as ReduxProvider} from 'react-redux';
 import store from './store';
 import notifee from '@notifee/react-native';
 import {getRoute} from './notifications/routes';
 import * as RootNavigation from './RootNavigation';
-import {parseDeepLink} from './components/ParseDeepLink';
-import {DeepLinkRequest} from './components/ParseDeepLink/models';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {setupPlayer} from './audio';
 import SwitchComponent from './navigation/SwitchComponent';
-import {Credentials} from './credentials';
 import {RealmProvider} from '@realm/react';
 import {UserSchemaRO} from './db/schemas/UserSchema';
 
@@ -21,7 +18,7 @@ function LMChatApp(): JSX.Element {
       const initialNotification = await notifee.getInitialNotification();
 
       if (initialNotification) {
-        let routes = getRoute(initialNotification?.notification?.data?.route);
+        const routes = getRoute(initialNotification?.notification?.data?.route);
         setTimeout(() => {
           RootNavigation.navigate(routes.route, routes.params);
         }, 1000);
@@ -40,11 +37,11 @@ function LMChatApp(): JSX.Element {
 
   return (
     <RealmProvider schema={[UserSchemaRO]}>
-      <GestureHandlerRootView style={{flex: 1}}>
+      <GestureHandlerRootView style={styles.flexStyling}>
         <ReduxProvider store={store}>
           <KeyboardAvoidingView
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={{flex: 1}}>
+            style={styles.flexStyling}>
             <SwitchComponent />
           </KeyboardAvoidingView>
         </ReduxProvider>
@@ -52,5 +49,11 @@ function LMChatApp(): JSX.Element {
     </RealmProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  flexStyling: {
+    flex: 1,
+  },
+});
 
 export default LMChatApp;
