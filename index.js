aeaee5dc545d0f241e1621e78e195da80943cfcc
likeminds <likeminds@likemindss-MacBook-Pro-3.local>
2023-12-22 14:45:24 +0530

    changed LMChatClient flow

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
-import {LMChatClient} from '@likeminds.community/chat-rn';
+import {ConversationState, LMChatClient} from '@likeminds.community/chat-rn';
 import notifee, {EventType} from '@notifee/react-native';
 import getNotification from './ChatSX/notifications';
 import {getRoute} from './ChatSX/notifications/routes';
@@ -32,8 +32,7 @@ messaging().setBackgroundMessageHandler(async remoteMessage => {
 });
 
 const myClient = LMChatClient.setApiKey('')
-  .setPlatformCode('rn')
-  .setVersionCode(parseInt('23'))
+  .setfilterStateConversation([])
   .build();
 
 function HeadlessCheck({isHeadless}) {
